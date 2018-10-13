23f12201353498cb1f231bdb5b46e9acdf3c8d4d
chenchuan <chenc224@163.com>
2018-10-13 12:55:38 +0800

    帐户信息数据

--- tex/makepdf.py
+++ tex/makepdf.py
@@ -8,10 +8,18 @@ import sys
 import time
 import jinja2
 
+def cpt(t):
+	import json
+	print(json.dumps(t,encoding="gbk",ensure_ascii=False))
+
 class main:
 	def __init__(self):
 		self.main()
 	def main(self):
+		self.field={}
+		for l in file("../data/field.txt","r").readlines():
+			d=l.decode("gbk").split("|")
+			self.field[d[0]]=[d[1],d[2],d[3],d[4],d[5],d[6]]
 		for i in range(100):
 			if hasattr(main,"rd%d" %(i)):
 				getattr(main,"rd%d" %(i))(self)
@@ -25,5 +33,25 @@ class main:
 		rd["servicetype"]=[]
 		for l in file("../data/servicetype.txt","r").readlines():
 			rd["servicetype"].append(l.decode("gbk").split("|"))
+	def rd2(self):	#处理帐户信息数据
+		rd["st001"]=[]
+		for l in file("../data/st001.txt","r").readlines():
+			d=l.decode("gbk").split("|")
+			print(d)
+			f=self.field[d[0]]
+			print(f)
+			h=[]
+			if d[1][0]=="Y":
+				h.append(d[0]+"*")
+			else:
+				h.append(d[0])
+			h.append(f[0])
+			if f[1]=="N":
+				h.append("%s%s.%s" %(f[1],f[2],f[3]))
+			else:
+				h.append("%s%s" %(f[1],f[2]))
+			h.append(f[4])
+			h.append(f[5])
+			rd["st001"].append(h)
 rd={}
 main()
