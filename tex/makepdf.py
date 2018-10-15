75d64da485fcf13d940e91f5c22faccb18137548
chenchuan <chenc224@163.com>
2018-10-15 21:06:52 +0800

    ͳһ´¦Àí¸÷ҵÎñÊý¾ÝÏî

--- tex/makepdf.py
+++ tex/makepdf.py
@@ -33,25 +33,26 @@ class main:
 		rd["servicetype"]=[]
 		for l in file("../data/servicetype.txt","r").readlines():
 			rd["servicetype"].append(l.decode("gbk").split("|"))
-	def rd2(self):	#处理帐户信息数据
-		rd["st001"]=[]
-		for l in file("../data/st001.txt","r").readlines():
-			d=l.decode("gbk").split("|")
-			print(d)
-			f=self.field[d[0]]
-			print(f)
-			h=[]
-			if d[1][0]=="Y":
-				h.append(d[0]+"*")
-			else:
-				h.append(d[0])
-			h.append(f[0])
-			if f[1]=="N":
-				h.append("%s%s.%s" %(f[1],f[2],f[3]))
-			else:
-				h.append("%s%s" %(f[1],f[2]))
-			h.append(f[4])
-			h.append(f[5])
-			rd["st001"].append(h)
+	def rd2(self):	#处理各业务的数据项
+		for yw in ["001","101"]:
+			rd["st"+yw]=[]
+			for l in file("../data/st%s.txt" %(yw),"r").readlines():
+				d=l.decode("gbk").split("|")
+				print(d)
+				f=self.field[d[0]]
+				print(f)
+				h=[]
+				if d[1][0]=="Y":
+					h.append(d[0]+"*")
+				else:
+					h.append(d[0])
+				h.append(f[0])
+				if f[1]=="N":
+					h.append("%s%s.%s" %(f[1],f[2],f[3]))
+				else:
+					h.append("%s%s" %(f[1],f[2]))
+				h.append(f[4])
+				h.append(f[5])
+				rd["st"+yw].append(h)
 rd={}
 main()
