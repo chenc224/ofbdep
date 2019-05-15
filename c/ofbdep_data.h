f399f68c07cedd7308113a7179656c8260327291
chenc224 <31564239+chenc224@users.noreply.github.com>
2019-05-15 21:39:18 +0800

    变量名调整。增加文件头描述

--- c/ofbdep_data.h
+++ c/ofbdep_data.h
@@ -4,13 +4,21 @@ struct stru_ofbdep_field	{	//字段数据字典表
 	char desc[100];			//描述
 };
 
-struct stru_datafile_field{//文件和字段的对应关系
+struct stru_ofbdep_datafile_field{//文件和字段的对应关系
 	char filemodel[3];//文件类型
 	int fieldcount;//字段数
 	int *field;
 };
 
+struct stru_ofbdep_datafilehead	{//数据文件头
+	char name[30],checkvalue[20];//名称，检查值,类型
+	int size,check;//长度,检查标志
+	char * sv;//指向字符串值
+	int * iv;//指向整型值
+};
+
 extern struct stru_ofbdep_field ofbdep_field[];	//全部字段数据字典表
 extern int ofbdep_field_count;					//全部字段数据字典表数量
-extern struct stru_datafile_field datafile_field[];//数据文件和字段对应关系
-extern int datafile_field_count;				//数据文件数量
+extern struct stru_ofbdep_datafile_field ofbdep_datafile_field[];//数据文件和字段对应关系
+extern int ofbdep_datafile_field_count;				//数据文件数量
+extern struct stru_ofbdep_datafilehead ofbdep_datafilehead[];//数据文件头
